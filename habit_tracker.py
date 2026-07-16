78263b08c56c1b70a3ff720044703ff83f9b7051
Sreevardhan Gullipalli <sreevardhang@MacBook-Air-von-Sreevardhan.local>
2026-07-16 15:30:50 +0530

    added user input

--- habit_tracker.py
+++ habit_tracker.py
@@ -1,1 +1,13 @@
-print("Habit Tracker")
+
+habits = []
+
+print('Habit Tracker')
+
+user_input = input('Enter a habit, format -- add <habit>: ')
+
+inputsplit = user_input.split()
+
+if inputsplit[0] == 'add':
+    habits.append(inputsplit[1])
+
+print("Habits: ", habits)
